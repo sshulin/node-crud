f6f3d6315bcb75c825da69484a3c6b8f8c88d3c3
Alexander Christphorov <hrst.work@gmail.com>
2018-03-21 22:06:30 +0700

    Added GET (one by id) route

--- routes/guests_routes.js
+++ routes/guests_routes.js
@@ -1,4 +1,20 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+	app.get('/guests/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {
+			'_id': new ObjectID(id)
+		};
+
+		db.collection('guests').findOne(details, (err, item) => {
+			if (err) {
+				res.send({'error': 'Something went wrong'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
 	app.post('/guests', (req, res) => {
 		const guest = {
 			forename: req.body.forename,
