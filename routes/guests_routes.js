d99592bf4c0e650b5ff2db94e2c2b5fc2cdeeb36
Alexander Christphorov <hrst.work@gmail.com>
2018-03-21 22:05:05 +0700

    Added CREATE route

--- routes/guests_routes.js
+++ routes/guests_routes.js
@@ -1,6 +1,18 @@
 module.exports = function(app, db) {
 	app.post('/guests', (req, res) => {
-		console.log(req.body);
-		res.send('Hello');
-	})
+		const guest = {
+			forename: req.body.forename,
+			surname: req.body.surname,
+			patronym: req.body.patronym,
+			brooms: req.body.brooms
+		};
+
+		db.collection('guests').insert(guest, (err, result) => {
+			if (err) {
+				res.send({'error': 'Something went wrong'});
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
+	});
 };
